2b2075b38a2d485ceb4b963eb6c327ac3cb514a5
rwb8 <rwb8@aber.ac.uk>
2023-03-07 10:51:28 +0000

    began work on training screen and top nav bar

---
+++ GUI/components/top_nav_bar.py
@@ -0,0 +1,35 @@
+from tkinter.ttk import *
+
+
+class TopNavigationBar(LabelFrame):
+    def __init__(self, master, controller, title, nav_button=None):
+        super().__init__(master, text="top navigation bar")
+
+        # content
+        # navigation button
+        if nav_button:
+            Button(
+                master=self,
+                text=nav_button.text,
+                image=nav_button.image,
+                command=nav_button.command
+            ).grid(
+                column=0,
+                row=0
+            )
+
+        # title
+        Label(
+            master=self,
+            text=title
+        ).grid(
+            column=1,
+            row=0
+        )
+
+
+class NavButtonData:
+    def __init__(self, text=None, image=None, command=None):
+        self.text = text
+        self.image = image
+        self.command = command
--- GUI/screens/training_screen.py
+++ GUI/screens/training_screen.py
@@ -1,9 +1,27 @@
 from tkinter import *
 
+import GUI.screens.model_screen
+from GUI.components.top_nav_bar import TopNavigationBar, NavButtonData
+
 
 class TrainingScreen(LabelFrame):
     def __init__(self, master, controller):
         super().__init__(master, text="training screen")
+        self.rowconfigure(0, weight=0)
+        self.rowconfigure(1, weight=1)
+        self.columnconfigure(0, weight=1)
 
         # content
-
+        TopNavigationBar(
+            master=self,
+            controller=controller,
+            title="Train New Model",
+            nav_button=NavButtonData(
+                text="back",
+                command=controller.show_frame(GUI.screens.model_screen.ModelScreen)
+            )
+        ).grid(
+            column=0,
+            row=0,
+            sticky="ew"
+        )
